1f869bda92383fc457ccbd6288172fd8a68890f9
kubix1605 <kubix1605@gmail.com>
2020-04-01 17:37:24 +0200

    experiment2

--- Assets/Resources/Scripts/AudioManager.cs
+++ Assets/Resources/Scripts/AudioManager.cs
@@ -4,7 +4,7 @@ using System;
 
 public class AudioManager : MonoBehaviour {
 
-
+    string filler = "AUDIOMANAGER";
     public Sound[] sounds;
     public AudioSource music;
     bool bossMusic = false;
---
+++ README.txt
@@ -0,0 +1,5 @@
+How to update your game on GitHub:
+
+1. git add -A
+2. git commit -m "*Your commit message*"
+3. git push origin master
